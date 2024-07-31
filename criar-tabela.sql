aa086b6284a9951cebd36e8992dd7bb849cf486c
GABRIELLA RODRIGUES LOPES <GABRIELLA0633@DIGITALCOLLEGE.LOCAL>
2024-07-31 11:55:04 -0300

    adições

--- criar-tabela.sql
+++ criar-tabela.sql
@@ -17,4 +17,14 @@ CREATE TABLE `projeto-back`.`usuarios` (
   `use_in_menu` VARCHAR(45) NULL,
   PRIMARY KEY (`id`));
 
-  
+  CREATE TABLE `projeto-back`.`produtos` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `enabled` TINYINT NULL DEFAULT 0,
+  `name` VARCHAR(45) NULL,
+  `slug` VARCHAR(45) NULL,
+  `use_in_menu` TINYINT NULL DEFAULT 0,
+  `stock` INT NULL DEFAULT 0,
+  `description` VARCHAR(45) NULL,
+  `price` FLOAT NULL,
+  `price_with_discount` FLOAT NULL,
+  PRIMARY KEY (`id`));
