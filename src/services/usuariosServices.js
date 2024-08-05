094e084d8976a2fa0d86739121573e351b85bea5
ANTONIEL FERREIRA SOUSA <ANTONIEL0867@DIGITALCOLLEGE.LOCAL>
2024-08-05 09:52:51 -0300

    Adicionei rota pra identificar usuario por ID

--- src/services/usuariosServices.js
+++ src/services/usuariosServices.js
@@ -1,60 +1,72 @@
 const model = require('../models/usuarios')
 
 const createUser = async (data) => {
-    try{
-    const {firstname, surname, email, password} = data
-    model.create({firstname, surname, email, password})
-} catch (error){
-    console.log('usuarios não cadastrado: '+ error)
-}
+    try {
+        const { firstname, surname, email, password } = data
+        model.create({ firstname, surname, email, password })
+    } catch (error) {
+        console.log('usuarios não cadastrado: ' + error)
+    }
 }
 
 const listUser = async () => {
-    try{
-    await model.sync()
-    const userList = await model.findAll()
-    return userList
-} catch (error){
-    console.log('erro ao listar usuarios: '+error)
-}
+    try {
+        await model.sync()
+        const userList = await model.findAll()
+        return userList
+    } catch (error) {
+        console.log('erro ao listar usuarios: ' + error)
+    }
 }
 
-const updateUser = async (data) =>{
-    try{
+const listUserId = async (id) => {
+    try {
+        await model.sync()
+        const userList = await model.findByPk(parseInt(id))
+        return userList
+    } catch (error) {
+        console.log('erro ao listar usuario: ' + error)
+    }
+}
+
+
+const updateUser = async (data) => {
+    try {
         const id = data.id
         const listaOBJ = Object.entries(data)
         const listaFiltrada = listaOBJ.filter(([chave, valor]) => valor !== '')
         const updateUser = Object.fromEntries(listaFiltrada)
         console.log(listaFiltrada)
-      await model.update(updateUser,{
-            where: {id: id}
+        await model.update(updateUser, {
+            where: { id: id }
         })
 
-    }catch (error){
-        console.log('Erro ao atualizar usuario: '+error)
+    } catch (error) {
+        console.log('Erro ao atualizar usuario: ' + error)
     }
 }
 
-const deleteUser =  async (data) => {
-   try{
-   const checkk = await model.destroy({
-        where: {id: data}
-    }) 
-    if (checkk > 0){
-        console.log('Usuario deletado')
-    }else{
-        console.log('Usuario não encontrado')
-    }
-    }catch{
+const deleteUser = async (data) => {
+    try {
+        const checkk = await model.destroy({
+            where: { id: data }
+        })
+        if (checkk > 0) {
+            console.log('Usuario deletado')
+        } else {
+            console.log('Usuario não encontrado')
+        }
+    } catch {
         console.log('Erro ao tentar deletar usuario')
     }
-} 
+}
 
-    
+
 
 module.exports = {
     createUser,
     listUser,
     updateUser,
-    deleteUser
+    deleteUser,
+    listUserId
 }
