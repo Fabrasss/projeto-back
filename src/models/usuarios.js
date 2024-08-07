6882e8423789f5bb1b7d376f4425d03703bd5bdd
ANTONIEL FERREIRA SOUSA <ANTONIEL0867@DIGITALCOLLEGE.LOCAL>
2024-08-07 11:15:30 -0300

    supabase

--- src/models/usuarios.js
+++ src/models/usuarios.js
@@ -1,7 +1,7 @@
 const { Sequelize, DataTypes } = require('sequelize');
 require('dotenv').config()
 const senha_postgre = process.env.senha_postgre
-const sequelize = new Sequelize('root_h78r','root_h78r_user',senha_postgre,{
+const sequelize = new Sequelize('postgresql://postgres.nrgysfupywxfpswtyoac:queroumdb123@aws-0-sa-east-1.pooler.supabase.com:6543/postgres',{
     host:'dpg-cqp1g90gph6c73ff38eg-a.oregon-postgres.render.com',
     dialect:'postgres',
     dialectOptions: {
@@ -39,6 +39,77 @@ const Usuarios = sequelize.define('usuarios', {
 });
 
 Usuarios.sync()
+
+const Categorias = sequelize.define('categorias', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    slug: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    use_in_menu: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false
+    }
+}, {
+    timestamps: true 
+});
+
+Categorias.sync()
+
+const Produtos = sequelize.define('produtos',{
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    enabled: {
+        type: DataTypes.BOOLEAN,
+        allowNull: true,
+        defaultValue: false 
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    slug: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    use_in_menu: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false
+    },
+    stock: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+        defaultValue: 0
+    },
+    description: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    price: {
+        type: DataTypes.FLOAT,
+        allowNull: false, 
+    },
+    price_with_descount: {
+        type: DataTypes.FLOAT,
+        allowNull: false
+    }
+},{
+     timestamps:true
+})
+Produtos.sync()
+
 
 sequelize.authenticate().then(function(){
     console.log('conexão com banco estabelecida')
