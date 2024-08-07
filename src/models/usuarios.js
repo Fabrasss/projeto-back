1918ba78c121d5ac96c51483e4dc635cb90479aa
Fabrasss <fabras595653@gmail.com>
2024-08-07 18:58:54 -0300

    Criei usuarios e tentando criar rotas

---
+++ src/models/usuarios.js
@@ -0,0 +1,31 @@
+const { Sequelize, DataTypes } = require('sequelize');
+
+const sequelize = new Sequelize('postgresql://postgres.fqvrpotctjxyrrryjnlg:adorocookiess!@aws-0-sa-east-1.pooler.supabase.com:6543/postgres') // Example for postgres
+
+const Usuarios = sequelize.define('usuarios', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    firstname: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    surname: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+}, {
+    timestamps: true 
+});
+
+Usuarios.sync()
